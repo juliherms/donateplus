179704f1bda0aee4c25219291e3631cc7e6c1b8d
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-04-07 23:14:58 -0300

    criacao do arquivo de carga inicial - data.sql

---
+++ donateplusapi/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- CARGA NA TABELA DE USUARIOS
+INSERT INTO TB_USER(name,username,email,password) VALUES('Juliherms Vasconcelos','juliherms','j.a.vasconcelos321@gmail.com','12345');
+
+-- CARGA NA TABELA DE CAMPANHAS
+
