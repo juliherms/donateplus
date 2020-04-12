87b948ad7330c415dddaa69766520ca59a044c16
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-04-12 07:59:59 -0300

    implementacao do eureka server - service discovery

--- eureka-server/src/main/java/com/donateplus/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/donateplus/eureka/EurekaServerApplication.java
@@ -2,8 +2,15 @@ package com.donateplus.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * This class reponsible to start eureka.
+ * @author j.a.vasconcelos
+ *
+ */
 @SpringBootApplication
+@EnableEurekaServer //allow eureka
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
