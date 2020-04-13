97f01429145d77eb12617a9857565bc4156478e5
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-04-12 23:07:04 -0300

    implementacao do servico de monitoramento

---
+++ donateplusmonitor/src/main/java/com/donateplus/monitor/DonateplusmonitorApplication.java
@@ -0,0 +1,23 @@
+package com.donateplus.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+/**
+ * Application responsible to monitor microservices
+ * @author j.a.vasconcelos
+ *
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class DonateplusmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonateplusmonitorApplication.class, args);
+	}
+
+}
---
+++ donateplusmonitor/src/test/java/com/donateplus/monitor/DonateplusmonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.donateplus.monitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DonateplusmonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
