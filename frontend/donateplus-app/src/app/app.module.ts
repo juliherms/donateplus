e592944fec23a48ea7062f72806a42e93c420d9a
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-04-19 21:29:32 -0300

    criacao dos componentes

--- frontend/donateplus-app/src/app/app.module.ts
+++ frontend/donateplus-app/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { ResendRegistratonTokenComponent } from './components/resend-registraton-token/resend-registraton-token.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginUserComponent,
+    RegisterUserComponent,
+    ResendRegistratonTokenComponent,
+    EditUserComponent,
+    ListUserComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/donateplus-app/src/app/components/resend-registraton-token/resend-registraton-token.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resend-registraton-token',
+  templateUrl: './resend-registraton-token.component.html',
+  styleUrls: ['./resend-registraton-token.component.sass']
+})
+export class ResendRegistratonTokenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
