96cf13d345b94088e40519bf0074453d7f15050e
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-04-19 22:21:16 -0300

    implementacao do header

--- frontend/donateplus-app/src/app/core/guards/auth.guard.ts
+++ frontend/donateplus-app/src/app/core/guards/auth.guard.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
+/**
+ * j.a.vasconcelos
+ * this class responsible to check token
+ */
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  {
-  
+export class AuthGuard implements CanActivate, CanLoad {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    return true;
+  }
 }
